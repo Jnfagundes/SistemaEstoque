63920be16738e8a85a1795c430a441c2f35522b9
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2024-09-02 12:32:47 -0300

    Reformulação da index e criação da lógica do login

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//Criação do objeto que recebe o usiario e a senha
+const usuarioLogin ={
+    username: "djrc",
+    password:"1234"
+};
+
+//função para fazer o login
+function login(){
+    const username = document.getElementById("usuario").value;
+    const password = document.getElementById("senha").value;
+
+    //Analisando o usuário e a senha digitada
+    if (username === usuarioLogin.username && password === usuarioLogin.password){
+        document.getElementById('telaLogin').style.display = "none";
+        document.getElementById('consultas').style.display = "block";
+    }
+    else{
+        alert("Usuário ou senha incorretos.");
+    }
+};
